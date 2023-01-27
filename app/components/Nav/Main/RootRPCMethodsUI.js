acd40cedaa1cbee6cd00ad88f824d202eee45731
Sylva Elendu <iamsylvaelendu@gmail.com>
2023-01-27 15:15:54 +0100

    initial commit

--- app/components/Nav/Main/RootRPCMethodsUI.js
+++ app/components/Nav/Main/RootRPCMethodsUI.js
@@ -409,7 +409,6 @@ const RootRPCMethodsUI = (props) => {
       )}
       {signType === 'typed' && (
         <TypedSign
-          navigation={props.navigation}
           messageParams={signMessageParams}
           onCancel={onSignAction}
           onConfirm={onSignAction}
