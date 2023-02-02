cb233ed5802a2f59011d5c1e683253936292685e
Owen Craston <owengc12@gmail.com>
2023-02-01 18:37:30 -0800

    fix wrong password error

--- app/components/Views/Login/index.js
+++ app/components/Views/Login/index.js
@@ -362,6 +362,8 @@ class Login extends PureComponent {
         );
         this.setState({ loading: false });
       } else if (toLowerCaseEquals(error, VAULT_ERROR)) {
+        const vaultCorruptionError = new Error('Vault Corruption Error');
+        Logger.error(vaultCorruptionError, strings('login.clean_vault_error'));
         this.setState({
           loading: false,
           error: strings('login.clean_vault_error'),
