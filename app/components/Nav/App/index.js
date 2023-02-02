d9c57a92e8c9a96ab4b4b1013374367db7ac1599
Owen Craston <owengc12@gmail.com>
2023-02-02 12:51:46 -0800

    wip get prev screen

--- app/components/Nav/App/index.js
+++ app/components/Nav/App/index.js
@@ -198,6 +198,10 @@ const VaultRecoveryFlow = () => (
   >
     <Stack.Screen
       name={Routes.VAULT_RECOVERY.RESTORE_WALLET}
+      // options={{ unmountOnBlur: true }}
+      // listeners={({ navigation }) => ({
+      //   blur: () => navigation.setParams({ previousScreen: undefined }),
+      // })}
       component={RestoreWallet}
     />
     <Stack.Screen
